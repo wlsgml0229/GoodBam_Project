9997523c3575e28ba50daa59655b53e699c15e42
안유진 <dbwlsldk0947@gmail.com>
2020-07-17 15:05:50 +0900

    안드로이드 안유진

---
+++ Android/GoodBam/app/src/main/java/com/example/goodbam/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.goodbam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import kotlinx.android.synthetic.main.activity_start.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+//        button.setOnClickListener{
+//            val intent = Intent(this,LoginPageActivity::class.java)
+//            startActivity(intent)
+//            overridePendingTransition(R.anim.fadein,R.anim.fadeout)
+//        }
+        val intent = Intent(this,LoginPageActivity::class.java)
+
+        Handler().postDelayed({
+            startActivity(intent)}
+            , 3000L)
+
+        Handler().postDelayed({
+            overridePendingTransition(R.anim.fadein,R.anim.fadeout)}
+            , 3000L)
+
+    }
+}
