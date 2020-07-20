032bca8545ec8c3acfabc1dd9ed968b938eadaf6
안유진 <dbwlsldk0947@gmail.com>
2020-07-20 10:01:11 +0900

    안유진-안드로이드 클라이언트

--- Android/GoodBam/app/src/main/java/com/example/goodbam/MainPageActivity.kt
+++ Android/GoodBam/app/src/main/java/com/example/goodbam/MainPageActivity.kt
@@ -3,9 +3,9 @@ package com.example.goodbam
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginPageActivity : AppCompatActivity() {
+class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.main_page)
     }
 }
--- Android/GoodBam/app/src/main/java/com/example/goodbam/PwsSearchActivity.kt
+++ Android/GoodBam/app/src/main/java/com/example/goodbam/PwsSearchActivity.kt
@@ -3,9 +3,9 @@ package com.example.goodbam
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginPageActivity : AppCompatActivity() {
+class PwsSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.pws_search)
     }
 }
